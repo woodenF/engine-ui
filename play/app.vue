dc36ec5295a16d832b2322ffd3a0bffa4957edcf
hao.feng <hao.feng@nx-engine.com>
2022-09-03 21:51:43 +0800

    fix: clean

--- play/app.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  测试
-  <Icon color="blue" :size="20">Hello Icon</Icon>
-</template>
--- play/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './app.vue';
-import { Icon } from '../dist/es';
-
-createApp(App)
-  .use(Icon)
-  .mount('#app');
