80f31a7911a4ca1f6edecc75f131d6dca844343b
woodenF <916885119@qq.com>
2022-09-19 14:52:07 +0800

    fix: 修复play启动问题

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import DefineOptions from 'unplugin-vue-define-options/rollup';
 
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx()
+    vueJsx(),
+    DefineOptions()
   ]
 });
